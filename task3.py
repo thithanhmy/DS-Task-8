19896d6fe5eaf2b38d5349fa1a9c2583266a25b4
thithanhmy <133974936+thithanhmy@users.noreply.github.com>
2023-05-19 20:27:12 +0100

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,5 @@
+# DS-Task-8
+
+# Creat a loop that will produce the desired output
+for i in range(1, 6):
+    print('*' * i)      #use symbol *  to repeat string, multiplying string with integers results in new string that repeats the original string
